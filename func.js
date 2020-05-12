c23a576e0205bab84c339bdd9787613e8a6bc526
Ningyuan Xu <xu.ningyuan@outlook.com>
2020-05-11 22:34:20 -0700

    progress

--- func.js
+++ func.js
@@ -1,4 +1,18 @@
 function addMessage() {
-	var text = document.getElementById("text").value;
-	document.getElementById("message-list").innerHTML = text;
+	parseAndAddContent();
+	document.getElementById("text").value = ""; // to clear the text in the text field
+	
+}
+
+
+function parseAndAddContent() {
+	let text = document.getElementById("text").value;
+	let li = document.createElement("li");
+	let textnode = document.createTextNode(text);
+	li.append(textnode);
+	if (text === "") {
+		alert("Please enter something into the text field!");
+	} else {
+		document.getElementById("my-list").append(li);
+	}
 }
