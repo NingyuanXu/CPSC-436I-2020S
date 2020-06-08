ec6336cf150be0c4ce7ead010a55bde766127771
Ningyuan Xu <xu.ningyuan@outlook.com>
2020-06-08 02:58:59 -0700

    finish

--- assignment3/backend/server.js
+++ assignment3/backend/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Data = require('./data');
 
-router.get('/messages', function(req, res){
+router.get('/texts', function(req, res){
   Data.find({}).then(function(texts){
         res.send(texts);
     });
     console.log('GET request');
 });
 
-//adding a new message -> input task attached to req.body
-router.post('/messages', function(req, res){
+router.post('/texts', function(req, res){
   Data.create(req.body).then(function(text){
         res.json(text);
     }).catch(error => {console.log('caught', error.text);
@@ -18,8 +17,7 @@ router.post('/messages', function(req, res){
     console.log('POST request', req.body);
 });
 
-//deleting list of messages
-router.delete('/messages', function(req, res){
+router.delete('/texts', function(req, res){
   Data.deleteMany({}, function(texts){
         res.send(texts);
     }).catch(error => {console.log('caught', error.texts)});
